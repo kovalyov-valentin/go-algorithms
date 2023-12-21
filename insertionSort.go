923f14465845a5a184816ca0ce4595e84f852779
kovalyov-valentin <kowalevvalen@yandex.ru>
2023-12-21 12:42:35 +0300

    added implement

--- insertionSort.go
+++ insertionSort.go
@@ -1,16 +1,12 @@
-package main 
+package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-
-func main() {
-	arr := []int{5, 1, 3, 7, 2, 4, 0, 9, 8, 6, 10}
+func insertionSortOne(arr []int) []int {
 	// from left to right
 	for i := 1; i < len(arr); i++ {
 		key := arr[i]
-		lst := i 
+		lst := i
 		// from right to left
 		for j := i - 1; j > -1; j-- {
 			if arr[j] < key {
@@ -20,7 +16,28 @@ func main() {
 			lst = j
 		}
 		arr[lst] = key
-		fmt.Println(arr)
+
 	}
+	return arr
+}
+
+func insertionSortTwo(arr []int) []int {
+	n := len(arr)
+
+	for i := 0; i < n; i++ {
+		for j := i; j > 0 && arr[j-1] > arr[j]; j-- {
+			arr[j], arr[j-1] = arr[j-1], arr[j]
+		}
+	}
+
+	return arr
+}
+
+func main() {
+	arr := []int{5, 1, 3, 7, 2, 4, 0, 9, 8, 6, 10}
+
+	fmt.Println("Insertion Sort №1:", insertionSortOne(arr))
+	fmt.Println("Insertion Sort №2:", insertionSortTwo(arr))
+
 
 }
